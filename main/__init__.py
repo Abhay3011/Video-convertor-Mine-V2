9650f06268e0ef5de53c24966a8b8ad449448c81
Abhay3011 <83626456+Abhay3011@users.noreply.github.com>
2023-01-04 22:19:01 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", "9544521", cast=int)
+API_ID = config("API_ID", "9544521")
 API_HASH = config("API_HASH", "5cf32e97dc94510e46524f2286c95116")
 BOT_TOKEN = config("BOT_TOKEN", "5635132664:AAGoJCbGdLxyjmy1wB7UUTMpXUVv2O60dN8")
 BOT_UN = config("BOT_UN", "dumpv4bot")
